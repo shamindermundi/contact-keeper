fceecefa62128c8822d7c8e262f70947603c8a9c
shamindermundi <punnumundi@gmail.com>
2020-04-14 21:39:53 +0530

    User model added

---
+++ models/User.js
@@ -0,0 +1,23 @@
+const moongoose = require("mongoose");
+
+const UserSchema = moongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = moongoose.model("user", UserSchema);
