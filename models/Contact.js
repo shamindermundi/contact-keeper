46b4773c943ed0522946d9c90bd04a013b91409a
shamindermundi <punnumundi@gmail.com>
2020-04-15 12:00:19 +0530

    Contact model

---
+++ models/Contact.js
@@ -0,0 +1,29 @@
+const moongoose = require("mongoose");
+
+const ContactSchema = moongoose.Schema({
+  user: {
+    type: moongoose.Schema.Types.ObjectId,
+    ref: "users",
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phone: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: "personal",
+  },
+  date: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+module.exports = moongoose.model("contact", ContactSchema);
