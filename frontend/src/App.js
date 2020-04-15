0c98955bd49bbc0640a9b374ba5d665084defb47
shamindermundi <punnumundi@gmail.com>
2020-04-15 14:44:04 +0530

    Home page added in app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 const App = () => {
-  return <div className="App">Contact keeper</div>;
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/" component={About} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
+  );
 };
 
 export default App;
